0ffbf25217c87c96a56dec43f0f0c0ca4baec9e1
TAC <tac@tac42.net>
2019-09-21 16:38:08 +0900

    Add basic convert test

--- tests/test_dbms_clickhouse.py
+++ tests/test_dbms_clickhouse.py
@@ -11,5 +11,17 @@ def test_return_empty_set():
     res = dbms_clickhouse.json2lcickhouse(src)
     assert res == expected
 
+def test_return_basic_type_and_values():
+    src = """
+    { "hello" : 42, "world" : 128.4, "bool" : true, "str" : "Hello,World" }
+    """
+
+    expected = [
+      {"hello" : "Float64", "world" : "Float64", "bool" : "UInt8", "str" : "String"},
+      {"hello" : 42,        "world" : 128.4,     "bool" : 1      , "str" : "Hello,World"}
+    ]
+    res = dbms_clickhouse.json2lcickhouse(src)
+    assert expected == res
+
 if __name__ == '__main__':
     pytest.main(['-v', __file__])
--- util/dbms_clickhouse.py
+++ util/dbms_clickhouse.py
@@ -1,9 +1,37 @@
-import time
+import json
 
-from dateutil.parser import parse
-from dateutil.tz import tzutc
+import re
+
+NumberPattern = re.compile('^\d+\.\d+$')
+
+def guess_type(value):
+    # Number?
+    if type(value) is float:
+        return ["Float64", float(value)]
+    if type(value) is int:
+        return ["Float64", int(value)]
+    if type(value) is bool:
+        v = 1 if value else 0
+        return ["UInt8", v]
+
+    return ["String", str(value)]
+
+
+def json2lcickhouse_sub(body, values, types):
+    for key, value in body.items():
+        (v, t) = guess_type(value)
+        values[key] = v
+        types[key] = t
+    return
 
 def json2lcickhouse(src_json_str, logger = None):
     """Convert json string to python dict with data types for Clickhouse"""
+    body = json.loads(src_json_str)
 
-    return [{},{}]
+    types = {}
+    values = {}
+
+    json2lcickhouse_sub(body, types, values)
+
+    return [types, values]
+

e85efa0b53a43f948e12be641e89b834af330570
TAC <tac@tac42.net>
2019-09-21 15:56:12 +0900

    Fix refactor

--- util/time_parser.py
+++ util/time_parser.py
@@ -3,16 +3,14 @@ import time
 from dateutil.parser import parse
 from dateutil.tz import tzutc
 
-
-
-def elastic_time_parse(str, logger = None):
-    """Parse str as time. Ignore nanosec order."""
+def elastic_time_parse(src, logger = None):
+    """Parse src string as datetime. Ignore nanosec order. If src is NOT valid format, return it self as string."""
     try:
-        ret = parse(str)
+        ret = parse(src)
         if ret.tzinfo == None:
             ret = ret.replace(tzinfo = tzutc())
 
     except ValueError as e:
-        ret = str
+        ret = src
 
     return ret

c6dabf458d5a2b9ceab24a4eb90008937fb27c2e
TAC <tac@tac42.net>
2019-09-25 08:10:09 +0900

    add sample implements

--- main.py
+++ main.py
@@ -15,9 +15,12 @@ def serialize_schema(types, source_id):
 
 def generate_new_table_name(source_id, schema_cache):
     tables = [ t for t in schema_cache.values() if t.startswith(source_id) ]
-    least_table = sorted(tables)[-1]
-    next_number = int(least_table[-3:]) + 1
-    next_number_idx = str(next_number).zfill(3)
+    next_idx = 1
+    if len(tables) > 0:
+        least_table = sorted(tables)[-1]
+        next_idx = int(least_table[-3:]) + 1
+
+    next_number_idx = str(next_idx).zfill(3)
     return source_id + "_" + next_number_idx
 
 
@@ -34,8 +37,12 @@ def insert_schema(client, source_id, data_table_name, serialized_schema):
     query = dbms_clickhouse.query_insert_schema_table_without_value()
     client.execute(query, [{"source_id": source_id, "schema": serialized_schema, "table_name": data_table_name}])
 
+def create_data_table(types, new_table_name):
+    query = dbms_clickhouse.query_create_data_table(types, new_table_name)
+    client.execute(query)
 
-def get_table_name_with_insert_if_new_schema(client, source_id, serialized, schema_cache):
+
+def get_table_name_with_insert_if_new_schema(client, source_id, types, serialized, schema_cache):
     if serialized in schema_cache.keys():
         return schema_cache[serialized]
 
@@ -49,65 +56,75 @@ def get_table_name_with_insert_if_new_schema(client, source_id, serialized, sche
     # it is true new schema !!
     new_table_name = generate_new_table_name(source_id, schema_cache)
     insert_schema(client, source_id, new_table_name, serialized)
+    create_data_table(types, new_table_name)
+    print("created new table {}".format(new_table_name))
 
     return new_table_name
 
+def insert_data(client, data_table_name, values):
+    query = dbms_clickhouse.query_insert_data_table_without_value(values.keys(), data_table_name)
+    client.execute(query, [values])
+
 # ------------------------------------------------
 
-source_id = "sosu"
+# source_id = "sosu"
 
-types, values = dbms_clickhouse.json2lcickhouse('{"helln":128, "w":{"orld":"world", "ow":[1,2,3]}}')
-serialized = serialize_schema(types, source_id)
-print(types)
-print(values)
-print(serialized)
-
-client = Client('192.168.11.200', 19000)
-create_schema_table(client) # init
-
-schema_cache = select_all_schemas(client) # {serialize_schema : table_name}
-
-new_table_name = get_table_name_with_insert_if_new_schema(client, source_id, serialized, schema_cache)
-print(new_table_name)
-
-# insert_schema(client, source_id, source_id + "_" + "042", serialized)
-# print(select_all_schemas(client))
-
+# types, values = dbms_clickhouse.json2lcickhouse('{"hoge":128, "w":{"orld":"world", "ow":[1,2,3]}}')
+# serialized = serialize_schema(types, source_id)
+# print(types)
+# print(values)
+# print(serialized)
 
 # client = Client('192.168.11.200', 19000)
-# print(client.execute('SHOW TABLES'))
-# client.execute(
-#     dbms_clickhouse.query_create_data_table(types, 'sample_001')
-# )
+# create_schema_table(client) # init
 
-# JST = timezone(timedelta(hours=+9), 'JST')
-# client.execute(
-#      'INSERT INTO {} (__uid, __collected_at, sample_f, sample_b) VALUES'.format("sample_data_table_001"),
-#      [{'__uid': 100, "__collected_at": datetime.now(JST), "sample_f":42.195, "sample_b": False}]
-#  )
+# schema_cache = select_all_schemas(client) # {serialize_schema : table_name}
 
-# import pika
-# import dbms_clickhouse
+# data_table_name = get_table_name_with_insert_if_new_schema(client, source_id, types, serialized, schema_cache)
+# insert_data(client, data_table_name, values)
 
-# import os
-# rabbitmq_host = os.environ.get('RABBITMQ_HOST') or "192.168.11.200" # for debug
-# rabbitmq_port = os.environ.get('RABBITMQ_PORT') or "5672"
+# ------------------------------------------
 
-# print(rabbitmq_host)
-# print(rabbitmq_port)
+import pika
+import os
+rabbitmq_host = os.environ.get('RABBITMQ_HOST') or "192.168.11.200" # for debug
+rabbitmq_port = os.environ.get('RABBITMQ_PORT') or "5672"
 
-# def callback(channel, method, properties, body):
-#     topic = method.routing_key.replace("/", "__")
-#     payload = str(body.decode('utf-8'))
+print(rabbitmq_host)
+print(rabbitmq_port)
 
-#     print(topic)
-#     print(payload)
+def callback(channel, method, properties, body):
+    topic = method.routing_key.replace("/", "__")
+    payload = str(body.decode('utf-8'))
 
-# connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
-# channel = connection.channel()
+    # print(topic)
+    # print(payload)
 
-# channel.queue_declare(queue='devic')
-# channel.basic_consume('devic', callback)
+    source_id = topic.replace('/', '_')
+    types, values = dbms_clickhouse.json2lcickhouse(payload)
+    # print(source_id)
 
-# print(' [*] Waiting for messages. To exit press CTRL+C')
-# channel.start_consuming()
+    # print(types)
+    # print(values)
+
+    serialized = serialize_schema(types, source_id)
+    data_table_name = get_table_name_with_insert_if_new_schema(client, source_id, types, serialized, schema_cache)
+    print(data_table_name)
+    print(serialized)
+
+    insert_data(client, data_table_name, values)
+
+# initialize rabbitmq
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitmq_host))
+channel = connection.channel()
+
+channel.queue_declare(queue='devic')
+channel.basic_consume('devic', callback)
+
+# initialize clickhouse
+client = Client('192.168.11.200', 19000)
+create_schema_table(client) # init
+schema_cache = select_all_schemas(client)
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()

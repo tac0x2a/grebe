01f4b19aea0877759add577883b58bab9dafa59e
TAC <tac@tac42.net>
2020-09-19 19:24:33 +0900

    Refactor grebe.py

--- grebe.py
+++ grebe.py
@@ -14,125 +14,147 @@ from br2dl.schema_store_yaml import SchemaStoreYAML
 from br2dl.schema_store_clickhouse import SchemaStoreClickhouse
 
 
-args = pa.parse_args()
-MQ_QNAME = args.queue_name
-MQ_HOST = args.mh
-MQ_POST = args.mp
-DB_HOST = args.dh
-DB_PORT = args.dp
-RETRY_MAX = args.retry_max_count
+class Grebe:
+    def __init__(self, channel, client, schema_store, specified_types, retry_max: int, tz_str: str, logger):
+        self.channel = channel
+        self.client = client
+        self.schema_store = schema_store
+        self.specified_types = specified_types
+        self.retry_max = retry_max
+        self.tz_str = tz_str
+        self.logger = logger
 
-SCHEMA_STORE = args.schema_store
-SCHEMA_DIR = args.local_schema_dir
+        self.schema_cache = self.schema_store.load_all_schemas()
+        self.logger.info(f"Load {len(self.schema_cache)} schemas from {self.schema_store}")
+        self.logger.info(f"Schemas: {[s for s in self.schema_cache.values()]}")
 
-TZ_STR = args.tz
+    def callback(self, channel, method, properties, body):
+        self.logger.debug("receive '{}({})'".format(method.routing_key, method.delivery_tag))
 
-# Logger initialize
-logger = logger.init_logger(args.log_level, args.log_format, args.log_file_size, args.log_file_count, args.log_file, "Grebe")
-logger.info(args)
+        try:
+            Grebe.__insert_data(
+                method, body,
+                self.schema_store,
+                self.schema_cache,
+                self.specified_types,
+                self.tz_str,
+                self.logger
+            )
 
+        except Exception as e:
+            self.logger.error(e, exc_info=e)
+            self.logger.error("Consume failed '{}'. retrying...".format(method.routing_key))
 
-# Retry processing
-def send_retry(channel, method, properties, body, logger, retry_max):
-    RetryCountKey = "x-grebe-retry-count"
+            import re
+            m = re.search("DB::Exception: Table (.+) doesn't exist..", e.message)
+            if m:
+                self.logger.error(f"Table '{ m.group(1) }' is renamed? Update schema table cache.")
+                self.schema_cache = store.load_all_schemas()
 
-    current_retry_count = 0
+            self.__send_retry(
+                channel, method, properties, body,
+                self.retry_max,
+                self.logger
+            )
 
-    if properties.headers and properties.headers.get(RetryCountKey):
-        current_retry_count = int(properties.headers.get(RetryCountKey))
+        finally:
+            channel.basic_ack(delivery_tag=method.delivery_tag)
+            self.logger.debug("return basic_ack '{}({})'".format(method.routing_key, method.delivery_tag))
 
-    if current_retry_count >= retry_max:
-        logger.error("Retry count exceeded!!({}). Discard Message = [exchange={}, routing_key={}, body={}]".format(retry_max, args.queue_name, method.routing_key, body))
-        return
+    @classmethod
+    def __insert_data(cls, method, body, schema_store, schema_cache, specified_types, tz_str, logger):
+        # replace delimiter in topic mqtt/amqp.
+        topic = method.routing_key
+        source_id = topic.replace("/", "_").replace(".", "_")
+        payload = str(body.decode('utf-8'))
 
-    props = pika.BasicProperties(
-        headers={RetryCountKey: current_retry_count + 1}
-    )
-    logger.debug("Re-sending [exchange={}, routing_key={}, props={}, body={}]".format(args.queue_name, method.routing_key, props, body))
-    channel.basic_publish(exchange=args.queue_name, routing_key=method.routing_key, properties=props, body=body)
-    logger.warning("Re-send complete. ({})".format(current_retry_count + 1))
+        if source_id in specified_types.keys():
+            spec_types = specified_types[source_id]
+        else:
+            spec_types = {}
 
+        (columns, types, values_list) = d2c.data_string2type_value(payload, specified_types=spec_types, tz_str=tz_str)
 
-def callback(channel, method, properties, body):
-    global schema_cache
-    global store
+        serialized = dbms.serialize_schema(columns, types, source_id)
+        data_table_name = dbms.get_table_name_with_insert_if_new_schema(client, schema_store, source_id, columns, types, serialized, schema_cache)
+        dbms.insert_data(client, data_table_name, columns, values_list)
 
-    logger.debug("receive '{}({})'".format(method.routing_key, method.delivery_tag))
+        logger.debug(serialized)
 
-    try:
-        insert_data(method, body, schema_cache, store, logger, tz_str=TZ_STR)
+    @classmethod
+    def __send_retry(cls, channel, method, properties, body, retry_max, logger):
+        RetryCountKey = "x-grebe-retry-count"
 
-    except Exception as e:
-        logger.error(e, exc_info=e)
-        logger.error("Consume failed '{}'. retrying...".format(method.routing_key))
+        current_retry_count = 0
 
-        import re
-        m = re.search("DB::Exception: Table (.+) doesn't exist..", e.message)
-        if m:
-            logger.error(f"Table '{ m.group(1) }' is renamed? Update schema table cache.")
-            schema_cache = store.load_all_schemas()
+        if properties.headers and properties.headers.get(RetryCountKey):
+            current_retry_count = int(properties.headers.get(RetryCountKey))
 
-        send_retry(channel, method, properties, body, logger, RETRY_MAX)
+        if current_retry_count >= retry_max:
+            logger.error("Retry count exceeded!!({}). Discard Message = [exchange={}, routing_key={}, body={}]".format(retry_max, args.queue_name, method.routing_key, body))
+            return
 
-    finally:
-        channel.basic_ack(delivery_tag=method.delivery_tag)
-        logger.debug("return basic_ack '{}({})'".format(method.routing_key, method.delivery_tag))
+        props = pika.BasicProperties(
+            headers={RetryCountKey: current_retry_count + 1}
+        )
+        logger.debug("Re-sending [exchange={}, routing_key={}, props={}, body={}]".format(args.queue_name, method.routing_key, props, body))
+        channel.basic_publish(exchange=args.queue_name, routing_key=method.routing_key, properties=props, body=body)
+        logger.warning("Re-send complete. ({})".format(current_retry_count + 1))
 
-
-def insert_data(method, body, schema_cache, store, logger, tz_str):
-    # replace delimiter in topic mqtt/amqp.
-    topic = method.routing_key
-    source_id = topic.replace("/", "_").replace(".", "_")
-    payload = str(body.decode('utf-8'))
-
-    if source_id in specified_types.keys():
-        spec_types = specified_types[source_id]
-    else:
-        spec_types = {}
-
-    (columns, types, values_list) = d2c.data_string2type_value(payload, specified_types=spec_types, tz_str=tz_str)
-
-    serialized = dbms.serialize_schema(columns, types, source_id)
-    data_table_name = dbms.get_table_name_with_insert_if_new_schema(client, store, source_id, columns, types, serialized, schema_cache)
-    dbms.insert_data(client, data_table_name, columns, values_list)
-
-    logger.debug(serialized)
 
 # --------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    args = pa.parse_args()
+    MQ_QNAME = args.queue_name
+    MQ_HOST = args.mh
+    MQ_POST = args.mp
+    DB_HOST = args.dh
+    DB_PORT = args.dp
+    RETRY_MAX = args.retry_max_count
 
-# initialize rabbitmq
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=MQ_HOST, port=MQ_POST))
-channel = connection.channel()
+    SCHEMA_STORE = args.schema_store
+    SCHEMA_DIR = args.local_schema_dir
 
-channel.queue_declare(queue=MQ_QNAME)
-channel.basic_consume(MQ_QNAME, callback)
-logger.info("RabbitMQ connected({}:{})".format(MQ_HOST, MQ_POST))
+    TZ_STR = args.tz
 
-# initialize clickhouse
-client = Client(DB_HOST, DB_PORT)
-logger.info("Clickhouse connected({}:{})".format(DB_HOST, DB_PORT))
+    # Logger initialize
+    logger = logger.init_logger(args.log_level, args.log_format, args.log_file_size, args.log_file_count, args.log_file, "Grebe")
+    logger.info(args)
 
-# Load Schema
-if SCHEMA_STORE == 'rdb':
-    store = SchemaStoreClickhouse(client)
-else:
-    schema_file = os.path.join(SCHEMA_DIR, f"schema_db_{DB_HOST}_{DB_PORT}.yml")
-    store = SchemaStoreYAML(schema_file)
+    # initialize rabbitmq
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=MQ_HOST, port=MQ_POST))
+    channel = connection.channel()
 
-schema_cache = store.load_all_schemas()
-logger.info(f"Load {len(schema_cache)} schemas from {SCHEMA_STORE}")
-logger.info(f"Schemas: {[s for s in schema_cache.values()]}")
+    channel.queue_declare(queue=MQ_QNAME)
+    logger.info("RabbitMQ connected({}:{})".format(MQ_HOST, MQ_POST))
 
-# Loading specified type file
-specified_types = {}
-if args.type_file:
-    try:
-        with open(args.type_file, 'r') as tf:
-            specified_types = yaml.load(tf)
-            logger.info(f"Load specified types. {specified_types}")
-    except (FileNotFoundError, yaml.parser.ParserError, yaml.scanner.ScannerError) as e:
-        logger.warning(f"type-file load failed. ('{args.type_file}') ignored specified types. ({e})")
+    # initialize clickhouse
+    client = Client(DB_HOST, DB_PORT)
+    logger.info("Clickhouse connected({}:{})".format(DB_HOST, DB_PORT))
 
-# start cousuming
-logger.info("Consuming ...")
-channel.start_consuming()
+    # Loading specified type file
+    specified_types = {}
+    if args.type_file:
+        try:
+            with open(args.type_file, 'r') as tf:
+                specified_types = yaml.load(tf)
+                logger.info(f"Load specified types. {specified_types}")
+        except (FileNotFoundError, yaml.parser.ParserError, yaml.scanner.ScannerError) as e:
+            logger.warning(f"type-file load failed. ('{args.type_file}') ignored specified types. ({e})")
+
+    # Load Schema
+    if SCHEMA_STORE == 'rdb':
+        store = SchemaStoreClickhouse(client)
+    else:
+        schema_file = os.path.join(SCHEMA_DIR, f"schema_db_{DB_HOST}_{DB_PORT}.yml")
+        store = SchemaStoreYAML(schema_file)
+
+    grebe = Grebe(channel, client, store, specified_types, RETRY_MAX, TZ_STR, logger)
+
+    def callback(channel, method, properties, body):
+        grebe.callback(channel, method, properties, body)
+
+    # start cousuming
+    logger.info("Consuming ...")
+    channel.basic_consume(MQ_QNAME, callback)
+    channel.start_consuming()

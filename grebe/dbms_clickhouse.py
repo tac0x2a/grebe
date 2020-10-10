46d09c992356b3a0a31ce41baebd9ed9bd2109fd
TAC <tac@tac42.net>
2020-10-10 21:49:17 +0900

    Add setup db name by passed

--- grebe/dbms_clickhouse.py
+++ grebe/dbms_clickhouse.py
@@ -15,8 +15,14 @@ class TableNotFoundException(Exception):
         self.nested = nested
 
 
-def dbms_client(db_host, db_port):
-    return Client(db_host, db_port)
+def dbms_client(db_host, db_port, db_name="default"):
+    client = Client(db_host, db_port)
+
+    use_db_query = f"USE {escape_symbol(db_name)}"
+    client.execute(use_db_query)
+    logger.info(f"created clickhouse db connection. db_name={db_name}")
+
+    return client
 
 
 # ---------------------------------------------------------------------
